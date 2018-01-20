858f5ff30666bb59916e8538c87fea29ba5d8235
darealex <darealex@developer.developer.developerbg.loc>
2018-01-20 21:31:27 +0200

    echo row fix

--- install/run.bash
+++ install/run.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+########################
+### RoboMen          ###
+### info@robomen.net ###
+########################
 
 clear
 #pwd
@@ -8,28 +12,29 @@ clear
 #PWD=$(pwd)
 #echo $PWD
 
-echo "debug result text: (1=yes or 0=not)"
+echo -e -n " Debug result: (1=yes or 0=not)"
 read -n 1 int_debug
 echo 
 #if [ $int_debug == 1 ]; then
     #echo "debugs"
-#fi  
-
+#fi   
 if [ $(whoami) != "root" ]; then
     echo "Not, User is root!"
+	echo
     exit 1
 fi
 
-echo "Debian Update System (1=yes or 0 = not)"
+echo -e -n "Debian Update System (1=yes or 0=not)"
 read -n 1 int_checkupdate
 echo
 if [ $int_checkupdate == 1 ]; then
-	echo " Default reinstall source.list ftp.bg.debian.org (1=default)"
+	echo -e -n " Default reinstall source.list ftp.bg.debian.org (1=default or 0=not)"
 	read -n 1 int_check
 	echo 
 	if [ $int_check == 1 ]; then
 		cp /etc/apt/sources.list  /etc/apt/sources.list.bak
 		cp sources.list.copy /etc/apt/sources.list
+		echo
 	fi
     arr_apt_command=(autoclean update upgrade dist-upgrade autoremove)
     for i in ${arr_apt_command[@]}; do
@@ -38,11 +43,12 @@ if [ $int_checkupdate == 1 ]; then
             echo "command: $bin_apt"
         else  
             echo `$bin_apt`
+			echo
         fi
     done
 fi 
-
-echo "Install Programs (1=yes or 0=not)"
+echo 
+echo -e -n "Install Programs (1=yes or 0=not)"
 read -n 1 int_checkprograms
 echo 
 if [ $int_checkprograms == 1 ]; then
@@ -55,20 +61,27 @@ if [ $int_checkprograms == 1 ]; then
             bin_apt_install="apt-get install ${i}"
             if [ $int_debug == 1 ]; then
                 echo "command: $bin_apt_install"
+				echo 
             else  
                 echo `$bin_apt_install`
+				echo 
             fi
         fi
+		echo
     done
 fi
 
-echo "Install Web Server (1=yes or 0=not)"
+echo
+echo -e -n "Install Web Server (1=yes or 0=not)"
 read -n 1 int_checkwebserver
 echo 
 if [ $int_checkwebserver == 1 ]; then
     source mysql.txt
+	echo
     source php7.0.txt
+	echo
     source apache2.txt
+	echo
 fi
 
 exit 0
